822e4d5f189443157b64e6f94156f998791623f0
Martin Kleppe <kleppe@ubilabs.net>
2020-11-18 00:00:38 +0100

    handle undefined ascii values

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const characters = ' -=oO0@';
 const emptyLine = ' '.repeat(16 * 2 + 1);
 
-code = process.argv[2] || 'sin(t-sqrt((x-7.5)**2+(y-6)**2))';
+code = process.argv[2] || 'cos(t+i+x*y)';
 
 console.clear();
 let output = '';
@@ -44,7 +44,7 @@ function render() {
 
       const character = characters[
         Math.floor(value * characters.length)
-      ];
+      ] || ' ';
 
       output += color + character + ' ';
       index++;
